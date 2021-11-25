e66a450d3c6e6f452f286454384af15159e743a5
cjy00n <cjy921004@gmail.com>
2021-11-25 15:27:23 +0900

    콘텐츠수정

--- app/src/main/java/com/example/welt/Content_HighRiskPregnancyTest.kt
+++ app/src/main/java/com/example/welt/Content_HighRiskPregnancyTest.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
 import com.example.welt.databinding.FragmentContentHighRiskPregnancyTestBinding
-import com.example.welt.databinding.FragmentContentWeekFeaturesBinding
 
 
 class Content_HighRiskPregnancyTest : Fragment() {
